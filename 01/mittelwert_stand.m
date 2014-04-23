46713bb8916087b8e4c08e811f2da68e7a218635
ze <ze>
2014-04-23 17:39:22 +0200

    m-file verbessert

--- 01/mittelwert_stand.m
+++ 01/mittelwert_stand.m
@@ -1,21 +1,28 @@
-%berechnet mittelwert und standardabweichung (f√r die aufgabe 1b). werds vllt noch erweitern
+%berechnet mittelwert und standardabweichung
+%arg 1 ist der name der datei (in "gnuplot"-formatierung). der name muss in
+%'' gesetzt werden. arg 2 ist die nummer der spalte, die man betrachten
+%will. kp ob das so intelligent ist, da matlab in der darstellung der tabelle noch eine zehnerpotenz ausklammert und die scheinbar nicht mehr zu sehen ist.
 
-function mittelwert_stand(x)
+function mittelwert_stand(ST,spalte)
+	format long;
+	A=load(ST);
+	x = A(:,spalte);
+	n = length(x);
 	mi = 0;
-	for k=1:length(x)
+	for k=1:n
 		mit = mi + x(k);
 		mi = mit;
 	end
-	mittel = (mi/length(x));
+	mittel = (mi/n);
 	s = 0;
-	for k=1:length(x)
+	for k=1:n
 		si =s + (x(k) -mittel)^2;		
 		s = si;
 	end
-	sigma = sqrt((1/(length(x)-1))*s);
+	sigma = sqrt((1/(n-1))*s);
 	mittel
 	sigma
-	fehler = sigma/sqrt(length(x))
-	min = min(x)
-	max = max(x)
+	fehler = sigma/sqrt(n)
+	minimum = min(x)
+	maximum = max(x)
 end
